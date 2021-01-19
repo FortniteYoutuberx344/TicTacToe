3c224f86797305ec1a2851ff195ac0aea4231ecb
bigyosh <minhansd@yahoo.com>
2021-01-18 20:52:24 -0800

    Finished main game.  Needs improvements for user convenience.

--- Tic Tac Toe.py
+++ Tic Tac Toe.py
@@ -4,8 +4,16 @@ import random #this is used to generate random numbers for moves of the AI
 symbol = input("Choose X or O: ").capitalize()
 symbol = symbol.strip()
 print('You chose ' + symbol)
-states = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
 
+states = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+winconditions = [[0, 1, 2],
+                 [3, 4, 5],
+                 [6, 7, 8],
+                 [0, 3, 6],
+                 [1, 4, 7],
+                 [2, 5, 8],
+                 [0, 4, 8],
+                 [2, 4, 6]]
 def printboard(state):
     print('---')
     boardline=''
@@ -17,46 +25,38 @@ def printboard(state):
     print('---')
 
 def move(character):
-    if character == 'X':
+    if character == symbol:
         move = input('Choose a location for your move (one of the numbers): ')
         move = int(move.strip())
-        states[move-1] = 'X'
+        states[move-1] = character
     else:
         picked = False
         while picked == False:
             move = random.randint(0, 8)
             if states[move] != 'O' and states[move] != 'X':
-                states[move] = 'X'
-                picked = True
-            else:
-                continue
-    if character == 'O':
-        move = input('Choose a location for your move (one of the numbers): ')
-        move = int(move.strip())
-        states[move-1] = 'O'
-    else:
-        picked = False
-        while picked == False:
-            move = random.randint(0, 8)
-            if states[move] != 'O' and states[move] != 'X':
-                states[move] = 'O'
+                states[move] = character
                 picked = True
             else:
                 continue
 
 printboard(states)
-states = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+states = ['.', '.', '.', '.', '.', '.', '.', '.', '.']
 
-won = False
 
-while won == False:
-
+while True:
     print("X turn")
-    if symbol == 'X':
-        move('X')
+    move('X')
     printboard(states)
 
     print("O turn")
-    if symbol == 'O':
-        move('O')
+    move('O')
     printboard(states)
+
+    for wincondition in winconditions:
+        winchecker = states[wincondition[0]] + states[wincondition[1]] + states[wincondition[2]]
+        if winchecker == "XXX":
+            print('X wins!')
+            quit()
+        elif winchecker == "OOO":
+            print("O wins")
+            quit()
